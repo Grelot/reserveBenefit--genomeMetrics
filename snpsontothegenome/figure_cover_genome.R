474ec063456b0727d3dec5a84a947acc5c48d224
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-10-22 16:56:13 +0200

    making the legends larger to be readable

--- snpsontothegenome/figure_cover_genome.R
+++ snpsontothegenome/figure_cover_genome.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(plyr)
 library(reshape)
+library(reshape2)
 library(gridExtra)
 
 cover_genome_figures <- function(species_name,titre1,titre2) {
@@ -31,8 +32,8 @@ cover_genome_figures <- function(species_name,titre1,titre2) {
   sc.left=c()
   for(sc in unique(dd.w$scaffold)){
     sc.dd=dd.w[which(dd.w$scaffold==sc),]
-    sc.left=c(sc.left,min(sc.dd$windows))
-    sc.right=c(sc.right,max(sc.dd$windows))
+    sc.left=c(sc.left, min(sc.dd$windows))
+    sc.right=c(sc.right, max(sc.dd$windows))
     sc.sca=c(sc.sca,sc)
   }
   scadd=data.frame(scaffold=sc.sca,left=sc.left,right=sc.right)
@@ -47,13 +48,13 @@ cover_genome_figures <- function(species_name,titre1,titre2) {
   rectangles.toprint=rectangles[row_id.tokeep,]
   dd.wg=dd.w[which(dd.w$scaffold %in% scadd.1$scaffold),]
   dm.wing=dm.win[which(dm.win$scaffold %in% scadd.1$scaffold),]
-  dm.winga=aggregate(meanDepth ~  windows, data = dm.wing, mean)
+  dm.winga=aggregate(meanDepth ~ windows, data = dm.wing, mean)
   covers.wing=covers.win[which(covers.win$scaffold %in% scadd.1$scaffold),]
   variant.wing=variant.win[which(variant.win$scaffold %in% scadd.1$scaffold),]
   ## [!] very long (write a file of 100000000 rows)
   dv.wing <- melt(covers.wing, id=(c("scaffold", "left","right","windows")))
   ## filter outliers variant with very high coverage
-  dv.wing.cutoff=dv.wing[which(dv.wing$value < 300),]
+  dv.wing.cutoff=dv.wing[which(dv.wing$value < 200),]
 
   ## plot figures
   p_numbervar=ggplot() +
@@ -63,16 +64,18 @@ cover_genome_figures <- function(species_name,titre1,titre2) {
     xlab("")+ylab("Number of variants")+ggtitle(titre1)+ylim(0, 100)+
     scale_x_continuous( labels=function(x) round(x/2.5,digits=0) )+
     theme_classic()
-
-
   p_depthcov=ggplot() +
-    geom_rect(data=rectangles.toprint, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax+200), 
+    geom_rect(data=rectangles.toprint, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax+100), 
               fill='gray80', alpha=0.8)+  
     geom_point(data=dv.wing.cutoff,aes(x=windows,y=value),size=0.05,alpha=0.3,color="royalblue")+
     geom_line(data=dm.winga,aes(x=windows,y=meanDepth),size=0.6,color="black")+
-    xlab("Base position (Mb)")+ylab("Depth coverage")+ggtitle(titre2)+ylim(0, 300)+
-    scale_x_continuous( labels=function(x) round(x/2.5,digits=0) )+
-    theme_classic()
+    xlab("Base position (Mb)")+ylab("Depth coverage")+ggtitle(titre2)+ylim(0, 200)+
+    scale_x_continuous( labels=function(x) round(x/2.5,digits=0))+
+    theme_classic()+
+    theme(
+      plot.title = element_text(size = 24),
+      axis.title = element_text(size = 20),
+      axis.text = element_text(size = 18))
   return(list(p_numbervar,p_depthcov))
 }
 
