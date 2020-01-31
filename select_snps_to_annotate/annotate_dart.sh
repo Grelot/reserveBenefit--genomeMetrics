1e38e9d418232ced8816e326eee2e311b09804cd
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-01-31 16:29:32 +0100

    dart script

---
+++ select_snps_to_annotate/annotate_dart.sh
@@ -0,0 +1,4 @@
+## load an environment with bedtools vcftools available
+SINGULARITY_SIMG="/media/superdisk/utils/conteneurs/snpsdata_analysis.simg"
+singularity shell --bind /media/superdisk:/media/superdisk $SINGULARITY_SIMG
+
